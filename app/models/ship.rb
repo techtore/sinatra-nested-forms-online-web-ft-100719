c0f9eec290386ac6f4d034081abec1171a8a4f2f
Victoria Hale <vic.hale74@gmail.com>
2019-11-20 00:22:16 +0000

    Automatically backed up by Learn

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,19 @@
 class Ship
+  
+  @@all = []
+  
+  def initialize(name, type, booty)
+    @name = params[:name]
+    @type = params[:type]
+    @booty = params[:booty]
+    @@all << self
+  end
+  
+  def self.all 
+    @@all 
+  end
+  
+  def self.clear 
+    @@all.destroy
+  end
 end
